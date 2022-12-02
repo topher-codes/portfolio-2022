69fb6d354fb81a40db08bfcbc6659957591fd4e3
Chris <ghost@drednaut.net>
2022-12-02 09:18:43 -0800

    added blog
    Signed-off-by: Chris <ghost@drednaut.net>

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,3 +1,41 @@
-export default function BlogPage() {
-	return <h1>Blog</h1>;
+import fs from 'fs';
+import matter from 'gray-matter';
+import Link from 'next/link';
+import NextLink from 'next/link';
+import path from 'path';
+import { postFilePaths, POSTS_PATH } from '../lib/mdxUtils';
+
+export default function Blog({ posts }: any) {
+	return (
+		<div>
+			<h1>Posts</h1>
+			<ul>
+				{posts.map((post: any) => (
+					<li key={post.filePath}>
+						<NextLink
+							as={`/blog/${post.filePath.replace(/\.mdx?$/, '')}`}
+							href={`/blog/[slug]`}
+						>
+							{post.data.title} - {post.data.description}
+						</NextLink>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export function getStaticProps() {
+	const posts = postFilePaths.map((filePath) => {
+		const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
+		const { content, data } = matter(source);
+
+		return {
+			content,
+			data,
+			filePath,
+		};
+	});
+
+	return { props: { posts } };
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 export default function ProjectPage({ repos }: any) {
 	return (
 		<div>
-			<h1>Projects</h1>
+			<h1>Projects: {repos.length}</h1>
 			{repos.map((p: any) => (
 				<div className="card" key={p.name}>
-					<Link href={`/projects/${p.name}`} key={p.name}>
+					<Link href={`/projects/${p.name}`} key={p.id}>
 						<p>{p.name}</p>
 						<p>{p.description}</p>
 						<p>Stars: {p.stargazers_count}</p>
