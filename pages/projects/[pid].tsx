d1e65d15848e9655278242d39fb0a34c8bbda5a5
Chris <ghost@drednaut.net>
2022-12-03 20:22:09 -0800

    fixed unsupported character >
    Signed-off-by: Chris <ghost@drednaut.net>

--- pages/projects/[pid].tsx
+++ pages/projects/[pid].tsx
@@ -9,7 +9,7 @@ const Project = () => {
 		<div>
 			<h2>Project: {pid}</h2>
 			<div className="card">
-				Link to project ---> &nbsp;
+				Link to project --- &nbsp;
 				<Link href={`https://github.com/halcyon-dayz/${pid}`} target="_blank">
 					{pid}
 				</Link>{' '}
