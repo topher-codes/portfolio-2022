b308ae4e4161722325747276f9b3462066423da3
Chris <ghost@drednaut.net>
2022-12-11 17:56:50 -0800

    added analytics
    Signed-off-by: Chris <ghost@drednaut.net>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Navbar>
 			<DefaultSeo {...SEO} />
 			<Component {...pageProps} />
+			<Analytics />
 		</Navbar>
 	);
 }
