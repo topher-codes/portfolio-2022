c18fafdd8d95427d6c9a25d53f297391c4edd1ca
Chris <ghost@drednaut.net>
2022-12-12 14:45:02 -0800

    updated styles
    Signed-off-by: Chris <ghost@drednaut.net>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar({ children }: any) {
 			</nav>
 			<div className="container">{children}</div>
 			<div className="footer">
-				<p>Made with ❤️ from Portland, OR</p>
+				<p className="text-white">Made with ❤️ from Portland, OR</p>
 			</div>
 		</main>
 	);
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 						<Link href="/special">Special ❤️</Link>
 					</nav>
 					<div className="container">{children}</div>
-					<div className="footer">
+					<div className="footer text-white">
 						<p>Made with ❤️ from Portland, OR</p>
 					</div>
 				</main>
